b5a9ebf0faaf97af479fc43f975763b65053356f
Manuel Chet <manuelchet117@gmail.com>
2021-10-27 21:48:21 -0600

    AVANCE

--- src/proyectoestadistica/ProyectoEstadistica.java
+++ src/proyectoestadistica/ProyectoEstadistica.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyectoestadistica;
+
+import ventanas.inicioE;
 
 /**
  *
@@ -15,7 +12,8 @@ public class ProyectoEstadistica {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        inicioE ini = new inicioE();
+        ini.setVisible(true);
         System.out.println("SIUUUUUUUUUUUUU");
     }
     
